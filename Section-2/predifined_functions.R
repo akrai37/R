7c4c475abffc9e69f5e5be5982304b4d984cb43d
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-06-15 20:00:31 +0530

    Add files via upload

---
+++ Section-2/predifined_functions.R
@@ -0,0 +1,37 @@
+
+#? It gives information about the function that we are using. It will be displayed in the help console. 
+
+?rnorm(5,10,8)
+
+x<-c("a","b","c")
+?c()
+
+A<-seq(from=10,to=20,along.with = x)
+?seq()
+
+B<-seq(from=10,to=20,length.out=100)
+B
+
+rep(5:6,each=10)
+rep(x,times=5)
+?rep()
+
+print()
+
+is.numeric()
+is.character()
+is.double()
+is.integer()
+
+typeof()
+
+sqrt()
+sqrt(A)
+paste()
+
+
+
+
+
+
+
