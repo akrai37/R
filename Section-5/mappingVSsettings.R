d40b378db57f24ad34b0578f1d3680e84881fc14
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-06-23 21:51:35 +0530

    Update mappingVSsettings.R

--- Section-5/mappingVSsettings.R
+++ Section-5/mappingVSsettings.R
@@ -25,5 +25,5 @@ r + geom_point(aes(size=10))
 #First, we need to understand that any aesthetic in ggplot2 (such as colour, size, shape, etc.) can be used in two distinct ways in your plots:
 #Option 1 - you can use the aesthetic to reflect some properties of your data. For example, clarity of the diamonds, like we did in the first example. This is called MAPPING an aesthetic.
 #Option 2 - you can choose a certain value for an aesthetic. For example, make the colour blue for ALL points or make the shape a square for ALL points. This is called SETTING an aesthetic and the keyword here is ALL.
-
+#The choice between mapping and setting is a trade-off. When mapping, you can convey more insights, whereas when setting, you get more control of how your chart looks.
 
